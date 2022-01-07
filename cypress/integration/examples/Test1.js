cb9d5481415dfb52635cb31c1ffcd304148d01ef
unknown <714512227@qq.com>
2022-01-07 19:00:30 +1100

    add all files

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,42 @@
+///<reference types = "Cypress" />
+
+describe('My First Test Suite',function(){
+    it('My FirstTest Case', function(){
+        //test step
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+        cy.get('.search-keyword').type('ca')
+        cy.wait(2000)
+        //select element from entire page
+        cy.get('.product').should('have.length', 5)
+        cy.get('.product:visible').should('have.length', 4)
+        //select element from target DOM
+        cy.get('.products').find('.product').should('have.length',4)
+        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+        //cy.contains('ADD TO CART')
+
+        cy.get('.products').find('.product').each(($el, index, $list) => {
+        
+            const textVeg = $el.find('h4.product-name').text()
+            if(textVeg.includes('Cashews'))
+            {
+                cy.wrap($el).find('button').click()
+            }
+        })
+
+        
+        cy.get('.brand').then(function(logoelement)
+        {
+            cy.log(logoelement.text())
+        })
+        const logo=cy.get('.brand')
+
+
+
+        
+    })
+
+   
+
+
+
+})
